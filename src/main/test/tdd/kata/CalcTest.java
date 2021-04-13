039c244ba0e84f8635b0700ea2de757e9ac353de
maksim.kuznetzoff2011 <maksim.kuznetzoff2011@gmail.com>
2021-04-13 12:33:44 +0300

    добавлен тест CalcTest

---
+++ src/main/test/tdd/kata/CalcTest.java
@@ -0,0 +1,13 @@
+package tdd.kata;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @Test
+    void sum() {
+        new Calc().sum();
+    }
+}
