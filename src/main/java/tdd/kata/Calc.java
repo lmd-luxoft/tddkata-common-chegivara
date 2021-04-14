3eb6e564a9f792078d31ceae717c4ca79b8c656a
maksim.kuznetzoff2011 <maksim.kuznetzoff2011@gmail.com>
2021-04-14 10:09:44 +0300

    добавлен тест

--- src/main/java/tdd/kata/Calc.java
+++ src/main/java/tdd/kata/Calc.java
@@ -1,11 +1,34 @@
 package tdd.kata;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Calc {
     public Integer sum(String s){
         try {
-            throw new UnsupportedOperationException();
+            Pattern pattern = Pattern.compile("^$|\\d+|-\\d+");
+            Matcher matcher = pattern.matcher(s);
+            int count = 0;
+            int sum=0;
+            if (!validate(s)) return -1;
+            while (matcher.find()) {
+                count++;
+                if(count>2)return  -1;
+                Integer sumPart= matcher.group().isEmpty()?0:Integer.valueOf(matcher.group());
+
+                sum=sum+sumPart;
+
+            }
+            if (count==0){return -1;}
+            return sum;
         }catch (Throwable e){
             return -1;
         }
+    }
 
+    private boolean validate (String sd ){
+        Pattern pattern = Pattern.compile("^-?[0-9]\\d*(,-?\\d+)?$|^$");
+        Matcher matcher = pattern.matcher(sd);
+        return  matcher.find();
     }
 }
